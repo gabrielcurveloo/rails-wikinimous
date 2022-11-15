45162c3399bd58520ecbd4433d72a39580bf8e0e
Gabriel Pereira <gabrielhcpereira688@gmail.com>
2022-11-15 18:17:48 +0000

    rails-wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Read all
+  get "articles", to: "articles#index"
+  # Create
+  get "articles/new", to: "articles#new", as: :new_article
+  post "articles", to: "articles#create"
+  # Read one
+  get "articles/:id", to: "articles#show", as: :article
+  # Update
+  get "articles/:id/edit", to: "articles#edit", as: :edit_article
+  patch "articles/:id", to: "articles#update"
+  # Delete
+  delete "articles/:id", to: "articles#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # resources :articles
 end
